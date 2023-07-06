2a77fed444fae7559d26b7b80e80a0c73dcc963b
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-07-07 00:08:02 +0800

    Create Client's route

---
+++ lib/screens/officer_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OfficerScreen extends StatelessWidget {
+  const OfficerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Officer Screen'),
+    );
+  }
+}
---
+++ lib/screens/staff_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class StaffScreen extends StatelessWidget {
+  const StaffScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Staff Screen'),
+    );
+  }
+}
