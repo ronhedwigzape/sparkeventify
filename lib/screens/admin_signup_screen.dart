b78557f2fa86d8a12048d736bfd63bfa7eabc491
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-07-07 00:06:15 +0800

    Create Admin screens

---
+++ lib/screens/admin_signup_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AdminSignupScreen extends StatelessWidget {
+  const AdminSignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Admin Signup Screen'),
+    );
+  }
+}
