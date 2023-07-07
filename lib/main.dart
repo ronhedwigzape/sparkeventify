dcb912ebc92cbeff06ca568885c5ee4b5383b975
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-07-07 17:48:49 +0800

    Refactor for auth changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,88 @@
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:provider/provider.dart';
 import 'package:student_event_calendar/firebase_options.dart';
-import 'package:student_event_calendar/platforms/admin.dart';
-import 'package:student_event_calendar/platforms/client.dart';
 import 'package:student_event_calendar/providers/user_provider.dart';
 import 'package:student_event_calendar/screens/admin_login_screen.dart';
+import 'package:student_event_calendar/screens/admin_screen.dart';
 import 'package:student_event_calendar/screens/client_login_screen.dart';
-import 'package:student_event_calendar/utils/colors.dart';
-enum AppPlatform { web, android, ios, unknown }
-
-AppPlatform getPlatform() {
-  if (kIsWeb) {
-    return AppPlatform.web;
-  } else if (Platform.isAndroid) {
-    return AppPlatform.android;
-  } else if (Platform.isIOS) {
-    return AppPlatform.ios;
-  } else {
-    return AppPlatform.unknown;
-  }
-}
-
-Widget getApp(AppPlatform platform) {
-  switch (platform) {
-    case AppPlatform.web:
-      return const Admin();
-    case AppPlatform.android:
-    case AppPlatform.ios:
-      return const Client();
-    default:
-      throw UnsupportedError('Unsupported platform');
-  }
-}
+import 'package:student_event_calendar/screens/officer_screen.dart';
+import 'package:student_event_calendar/screens/staff_screen.dart';
+import 'package:student_event_calendar/screens/student_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(getApp(getPlatform()));
+  runApp(MultiProvider(
+    providers: [ChangeNotifierProvider(create: (_) => UserProvider())],
+    child: const App(),
+  ));
 }
 
-Widget build(BuildContext context) {
-  return MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (_) => UserProvider()),
-    ],
-    child: MaterialApp(
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Student Event Calendar',
-      home: StreamBuilder(
-          stream: FirebaseAuth.instance.authStateChanges(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.active) {
-              if (snapshot.hasData) {
-                if (Platform.isAndroid) {
-                  return const Client();
-                } else if (Platform.isIOS) {
-                  return const Client();
-                } else if (kIsWeb) {
-                  return const Admin();
-                }
-              } else if (snapshot.hasError) {
-                return Center(
-                  child: Text('${snapshot.error}'),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const AuthScreen(),
+    );
+  }
+}
+
+class AuthScreen extends StatelessWidget {
+  const AuthScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (ctx, userSnapshot) {
+        if (userSnapshot.hasData) {
+          return FutureBuilder(
+            future: FirebaseFirestore.instance
+                .collection('users')
+                .doc(FirebaseAuth.instance.currentUser?.uid)
+                .get(),
+            builder: (ctx, AsyncSnapshot<DocumentSnapshot> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
                 );
               }
-            }
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(
-                  color: primaryColor,
-                ),
-              );
-            }
-            // login
-            if (kIsWeb) {
-              return const AdminLoginScreen();
-            } else if (Platform.isAndroid) {
-              return const ClientLoginScreen();
-            } else if (Platform.isIOS) {
-              return const ClientLoginScreen();
-            }
-            return const SizedBox();
-          }),
-    ),
-  );
+              
+              final String userType = snapshot.data?.get('userType');
+              if(userType == 'Admin' && kIsWeb){
+                  return const AdminScreen();
+              } else if (userType == 'Student' && (Platform.isAndroid || Platform.isIOS)) {
+                return const StudentScreen();
+              } else if (userType == 'Staff' && (Platform.isAndroid || Platform.isIOS)) {
+                return const StaffScreen();
+              } else if (userType == 'Officer' && (Platform.isAndroid || Platform.isIOS)) {
+                return const OfficerScreen();
+              } else {
+                return const Center(
+                  child: Text('Unknown user'),
+                );
+              }
+            },
+          );
+        }
+        if (Platform.isAndroid || Platform.isIOS) {
+          return const ClientLoginScreen();
+        } else {
+          return const AdminLoginScreen(); 
+        } 
+      },
+    );
+  }
 }
