36f6c68e3cfe869c7d1b1776775ace442eb0e6ca
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-07-09 02:43:09 +0800

    Create events and calendar widget

---
+++ lib/models/events.dart
@@ -0,0 +1,53 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Event {
+  final String eventName;
+  final DateTime eventDate;
+  final String eventDescription;
+  final String createdBy;
+  final String? eventImage;
+  final String eventDocument;
+  final List<dynamic>? attendees;
+  final String eventType;
+  final String status;
+
+  const Event({
+    required this.eventName,
+    required this.eventDate,
+    required this.eventDescription,
+    required this.createdBy,
+    this.eventImage,
+    required this.eventDocument,
+    this.attendees,
+    required this.eventType,
+    required this.status,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'eventName': eventName,
+        'eventDate': eventDate,
+        'eventDescription': eventDescription,
+        'createdBy': createdBy,
+        'eventImage': eventImage,
+        'eventDocument': eventDocument,
+        'attendees': attendees,
+        'eventType': eventType,
+        'status': status,
+      };
+
+  static Event fromSnap(DocumentSnapshot snap) {
+    var snapshot = snap.data() as Map<String, dynamic>;
+
+    return Event(
+      eventName: snapshot['eventName'],
+      eventDate: (snapshot['eventDate'] as Timestamp).toDate(),
+      eventDescription: snapshot['eventDescription'],
+      createdBy: snapshot['createdBy'],
+      eventImage: snapshot['eventImage'],
+      eventDocument: snapshot['eventDocument'],
+      attendees: snapshot['attendees'],
+      eventType: snapshot['eventType'],
+      status: snapshot['status'],
+    );
+  }
+}
---
+++ lib/widgets/calendar.dart
@@ -0,0 +1,46 @@
+// your imports...
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class Calendar extends StatefulWidget {
+  const Calendar({super.key});
+
+  @override
+  _CalendarState createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  late DateTime _selectedDay;
+
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      firstDay: DateTime.utc(2010, 10, 16),
+      lastDay: DateTime.utc(2030, 3, 14),
+      focusedDay: _focusedDay,
+      calendarFormat: _calendarFormat,
+      selectedDayPredicate: (day) {
+        return isSameDay(_selectedDay, day);
+      },
+      onDaySelected: (selectedDay, focusedDay) {
+        setState(() {
+          _selectedDay = selectedDay;
+          _focusedDay = focusedDay; 
+          _calendarFormat = CalendarFormat.month;
+        });
+      },
+      onFormatChanged: (format) {
+        if (_calendarFormat != format) {
+          setState(() {
+            _calendarFormat = format;
+          });
+        }
+      },
+      onPageChanged: (focusedDay) {
+        _focusedDay = focusedDay;
+      },
+    );
+  }
+}
