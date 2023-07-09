e21310be12bcc74be04fbb54f2b7fada0669c224
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-07-09 11:42:54 +0800

    Create Admin Screen Layout and add constant colors

--- lib/layouts/admin_screen_layout.dart
+++ lib/layouts/admin_screen_layout.dart
@@ -1,12 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:student_event_calendar/utils/colors.dart';
+import 'package:student_event_calendar/utils/global.dart';
+import 'package:student_event_calendar/widgets/cspc_logo.dart';
 
-class AdminScreenLayout extends StatelessWidget {
+class AdminScreenLayout extends StatefulWidget {
   const AdminScreenLayout({super.key});
 
   @override
+  State<AdminScreenLayout> createState() => _AdminScreenLayoutState();
+}
+
+class _AdminScreenLayoutState extends State<AdminScreenLayout> {
+   
+  int _page = 0;
+  PageController pageController = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController(); 
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
+  void navigationTapped(int page) {
+    setState(() {
+      _page = page;
+    });
+    pageController.jumpToPage(page);
+  }
+
+  void onPageChanged (int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+  
+  
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Admin Screen'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: tertiaryColor,
+        centerTitle: false,
+        title: const CspcLogo(
+          height: 30.0,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () => navigationTapped(0),
+            icon: Icon(
+              Icons.home,
+              color: _page == 0 ? primaryColor : secondaryColor,
+            ),
+          ),
+          IconButton(
+            onPressed: () => navigationTapped(1),
+            icon: Icon(
+              Icons.search,
+              color: _page == 1 ? primaryColor : secondaryColor,
+            ),
+          ),
+          IconButton(
+            onPressed: () => navigationTapped(2),
+            icon: Icon(
+              Icons.add_a_photo,
+              color: _page == 2 ? primaryColor : secondaryColor,
+            ),
+          ),
+          IconButton(
+            onPressed: () => navigationTapped(3),
+            icon: Icon(
+              Icons.favorite,
+              color: _page == 3 ? primaryColor : secondaryColor,
+            ),
+          ),
+          IconButton(
+            onPressed: () => navigationTapped(4),
+            icon: Icon(
+              Icons.person,
+              color: _page == 4 ? primaryColor : secondaryColor,
+            ),
+          ),
+        ],
+      ),
+      body: PageView(
+        controller: pageController,
+        onPageChanged: onPageChanged,
+        physics: const NeverScrollableScrollPhysics(),
+        children: homeScreenItems,
+      ),
     );
   }
 }
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -6,3 +6,5 @@ const Color blueColor = Color.fromARGB(255, 21, 29, 83);
 const whiteColor = Colors.white;
 const primaryColor = Colors.indigo;
 const secondaryColor = Colors.grey;
+const tertiaryColor = Colors.black;
+const Color lightGreyColor = Color.fromARGB(255, 246, 246, 246);
