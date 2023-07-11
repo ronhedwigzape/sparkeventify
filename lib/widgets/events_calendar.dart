07076ca8e5d2f69b9febd560721559bcc3828b0c
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-07-12 02:20:12 +0800

    Refactor events calendar widget
    Errors:
    - When it is clicked to a day in the calendar that has event, the error returns. `Unexpected null value`
    
    Changes:
    - The dialog is removed because after clicking the day, dialog is not showing.
    - Just scaffold the event details below the events calendar

--- lib/widgets/events_calendar.dart
+++ lib/widgets/events_calendar.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:student_event_calendar/models/events.dart';
 import 'package:student_event_calendar/resources/firestore_event_methods.dart';
+import 'package:student_event_calendar/utils/global.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class EventsCalendar extends StatefulWidget {
@@ -19,65 +19,72 @@ class EventsCalendarState extends State<EventsCalendar> {
   final fireStoreEventMethods = FireStoreEventMethods();
 
   @override
-  void initState() {
-    super.initState();
-    fireStoreEventMethods.getEvents().then((map) {
-      setState(() {
-        _events = map;
-      });
-      if (kDebugMode) {
-        print(_events);
-      }
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return TableCalendar(
-      eventLoader: (day) {
-        return _events[day] ?? [];
-      },
-      firstDay: DateTime.utc(2015, 01, 01),
-      lastDay: DateTime.utc(2030, 3, 14),
-      focusedDay: _focusedDay,
-      calendarFormat: _calendarFormat,
-      selectedDayPredicate: (day) {
-        return isSameDay(_selectedDay, day);
-      },
-      onDaySelected: (selectedDay, focusedDay) {
-        debugPrint('Day selected: $selectedDay');
-        setState(() {
-          _selectedDay = selectedDay;
-          _focusedDay = focusedDay;
-          _calendarFormat = CalendarFormat.month;
-        });
-        // TODO: Show events for the selected day
-        if (_events[selectedDay] != null) {
-          debugPrint('Events: ${_events[selectedDay]}');
-          if (_events[selectedDay] != null) {
-            WidgetsBinding.instance.addPostFrameCallback((_) {
-              // TODO: Show dialog if there are events for the selected day
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return const AlertDialog(
-                    title: Text('Simple Dialog'),
-                  );
+    return FutureBuilder<Map<DateTime, List<Event>>>(
+      future: fireStoreEventMethods.getEvents(), // Fetch events here
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return const Center(child: Text("Something went wrong!"));
+        } else {
+          _events = snapshot.data!;
+          return Column(
+            children: [
+              TableCalendar(
+                eventLoader: (day) {
+                  return _events[ignoreTime(day.toUtc())] ?? [];
                 },
-              );
-            });
-          }
+                firstDay: DateTime.utc(2015, 01, 01),
+                lastDay: DateTime.utc(2030, 3, 14),
+                focusedDay: _focusedDay,
+                calendarFormat: _calendarFormat,
+                selectedDayPredicate: (day) {
+                  return isSameDay(_selectedDay, day);
+                },
+                onDaySelected: (selectedDay, focusedDay) {
+                  if (_events[ignoreTime(selectedDay.toUtc())] != null &&
+                      _events[ignoreTime(selectedDay.toUtc())]!.isNotEmpty) {
+                    debugPrint('Day selected: $_events[ignoreTime(selectedDay.toUtc())]');
+                    setState(() {
+                      _selectedDay = selectedDay;
+                      _focusedDay = focusedDay;
+                      _calendarFormat = CalendarFormat.month;
+                    });
+                  }
+                },
+                onFormatChanged: (format) {
+                  if (_calendarFormat != format) {
+                    setState(() {
+                      _calendarFormat = format;
+                    });
+                  }
+                },
+                onPageChanged: (focusedDay) {
+                  _focusedDay = focusedDay;
+                },
+              ),
+              Expanded(
+                child: _selectedDay == null
+                    ? const Center(child: Text('No Events Today'))
+                    : _events[ignoreTime(_selectedDay!.toUtc())] != null
+                        ? ListView.builder(
+                            itemCount: _events[_selectedDay]!.length,
+                            itemBuilder: (context, index) {
+                              final event = _events[_selectedDay]![index];
+                              return ListTile(
+                                leading: Image.network(event.image!),
+                                title: Text(event.title),
+                                subtitle: Text(
+                                    'Type: ${event.type}\nVenue: ${event.venue}\nDescription: ${event.description}'),
+                              );
+                            },
+                          )
+                        : const Center(child: Text('No Events Today')),
+              ),
+            ],
+          );
         }
-      },
-      onFormatChanged: (format) {
-        if (_calendarFormat != format) {
-          setState(() {
-            _calendarFormat = format;
-          });
-        }
-      },
-      onPageChanged: (focusedDay) {
-        _focusedDay = focusedDay;
       },
     );
   }
