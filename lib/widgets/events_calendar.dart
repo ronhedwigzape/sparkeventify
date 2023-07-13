3ac72a06cf3e6b7b6f84a9eaf88940584a60f8eb
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-07-13 13:06:23 +0800

    Add upcoming events and past events

--- lib/widgets/events_calendar.dart
+++ lib/widgets/events_calendar.dart
@@ -19,12 +19,21 @@ class EventsCalendarState extends State<EventsCalendar> {
   Map<DateTime, List<Event>> _events = {};
   final fireStoreEventMethods = FireStoreEventMethods();
   late Future<Map<DateTime, List<Event>>> eventsFuture;
+  final Map<DateTime, List<Event>> _upcomingEvents = {};
+  final Map<DateTime, List<Event>> _pastEvents = {};
 
   @override
   void initState() {
     super.initState();
     eventsFuture = fireStoreEventMethods.getEventsByDate();
-    print('Events: $eventsFuture');
+    DateTime currentDate = DateTime.now();
+    _events.forEach((key, value) {
+      if (key.isAfter(currentDate)) {
+        _upcomingEvents.putIfAbsent(key, () => value);
+      } else {
+        _pastEvents.putIfAbsent(key, () => value);
+      }
+    });
   }
 
   List<Event> _getEventsForDay(DateTime day) {
@@ -52,7 +61,7 @@ class EventsCalendarState extends State<EventsCalendar> {
               TableCalendar(
                 eventLoader: (day) {
                   DateTime adjustedDay =
-                      DateTime(day.year, day.month, day.day, 0, 0, 0);
+                      DateTime(day.year, day.month, day.day, 0, 0, 0).toLocal();
                   return _getEventsForDay(adjustedDay);
                 },
                 firstDay: DateTime.utc(2015, 01, 01),
@@ -70,7 +79,7 @@ class EventsCalendarState extends State<EventsCalendar> {
                       0,
                       0,
                       0 // set the time to midnight
-                      );
+                      ).toLocal();
                   setState(() {
                     _selectedDay = adjustedSelectedDay;
                     _focusedDay = focusedDay;
@@ -88,60 +97,100 @@ class EventsCalendarState extends State<EventsCalendar> {
                   _focusedDay = focusedDay;
                 },
                 calendarBuilders: CalendarBuilders(
-                  defaultBuilder: (context, day, focusedDay) {
-                    DateTime adjustedDay =
-                        DateTime(day.year, day.month, day.day, 0, 0, 0);
+                  defaultBuilder: (context, dateTime, focusedDay) {
+                    DateTime adjustedDay = DateTime(dateTime.year, dateTime.month, dateTime.day, 0, 0, 0).toLocal();
                     if (_events.containsKey(adjustedDay)) {
                       return Container(
                         decoration: const BoxDecoration(
-                          color: primaryColor,
+                          color: lightIndigo,
                           shape: BoxShape.circle,
                         ),
+                        margin: const EdgeInsets.all(4.0),
                         alignment: Alignment.center,
                         child: Text(
-                          day.day.toString(),
-                          style: const TextStyle(color: Colors.white),
-                        ),
-                      );
-                    } else {
-                      return Container(
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                        ),
-                        alignment: Alignment.center,
-                        child: Text(
-                          day.day.toString(),
+                          dateTime.day.toString(),
                         ),
                       );
                     }
+                    return Container(
+                      margin: const EdgeInsets.all(4.0),
+                      alignment: Alignment.center,
+                      child: Text(
+                        dateTime.day.toString(),
+                      ),
+                    );
                   },
                 ),
               ),
               Expanded(
                 child: _selectedDay == null
                     // Returns if today has no events because this is the focused day
-                    ? const Center(child: Text('No Events Today'))
+                    ? const Center(child: Text('No Events for today. Enjoy your day!', style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),))
                     // Returns if there is an event on the selected day
                     : _events[_selectedDay!] != null
-                        ? ListView.builder(
-                            itemCount: _events[_selectedDay]?.length ?? 0,
-                            itemBuilder: (context, index) {
-                              final event = _events[_selectedDay]![index];
-                              final time = DateFormat('hh:mm a').format(event.time);
-                              return ListTile(
-                                leading: Image.network(event.image!),
-                                title: Text(event.title),
-                                subtitle: Text(
-                                    'Type: ${event.type}\nVenue: ${event.venue}\nDescription: ${event.description}\nTime: $time'),
-                              );
-                            },
-                          )
-                        : const Center(child: Text('No Events For This Day')),
+                        ? Column(
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.all(8.0),
+                              child: Text(
+                                'Events for this day', 
+                                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            Expanded(
+                              child: ListView.builder(
+                                  itemCount: _events[_selectedDay]?.length ?? 0,
+                                  itemBuilder: (context, index) {
+                                    final event = _events[_selectedDay]![index];
+                                    final time = DateFormat('hh:mm a').format(event.time);
+                                    return Card( 
+                                      margin: const EdgeInsets.all(8.0),  
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(20.0),
+                                        child: ListTile(
+                                          leading: Image.network(event.image!, width: 100.0,),
+                                          title: Text(event.title),
+                                          titleTextStyle: const TextStyle(
+                                            fontSize: 20.0,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                          subtitle: Text('Type: ${event.type}\nVenue: ${event.venue}'
+                                              '\nDescription: ${event.description}\nTime: $time'),
+                                          ),
+                                      ),
+                                      );
+                                    },
+                                  ),
+                            ),
+                          ],
+                        )
+                        // Returns if there is no event on the selected day
+                        : const Center(child: Text('No Events For This Day. Please update later for future events!', style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),)),
               ),
+              // Upcoming and Past Events
+              const Text('Upcoming Events'),
+              Expanded(child: _eventListView(_upcomingEvents)),
+              const Text('Past Events'),
+              Expanded(child: _eventListView(_pastEvents)),
             ],
           );
         }
       },
     );
   }
+}
+
+// Returns a list of events
+Widget _eventListView(Map<DateTime, List<Event>> eventMap) {
+  return ListView.builder(
+    itemCount: eventMap.values.length,
+    itemBuilder: (context, index) {
+      List<Event> eventList = eventMap.values.elementAt(index);
+      return ListTile(
+        title: Text(eventList[index].title),
+        subtitle: Text('${eventList[index].date}'),
+        trailing: Text(eventList[index].description),
+      );
+    }
+  );
 }
