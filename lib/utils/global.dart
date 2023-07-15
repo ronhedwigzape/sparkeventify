1f0466edc2e7fc76c14b8209fd2da447b5ef1554
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-07-15 15:03:18 +0800

    Refactor events calendar from widget to screen

--- lib/utils/global.dart
+++ lib/utils/global.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:student_event_calendar/screens/post_screen.dart';
 import 'package:student_event_calendar/screens/profile_screen.dart';
-import 'package:student_event_calendar/widgets/events_calendar.dart';
+import 'package:student_event_calendar/screens/events_calendar_screen.dart';
 
 // Constant variables for the app
 const webScreenSize = 600;
@@ -14,15 +14,16 @@ const appName = 'Announce';
 // Global key for the events calendar
 
 List<Widget> homeScreenItems = [
-  kIsWeb ?  const EventsCalendar() : const Center(child: Text('Home Screen')),
-  kIsWeb ? const PostScreen() : const EventsCalendar(),
+  kIsWeb
+      ? const EventsCalendarScreen()
+      : const Center(child: Text('Home Screen')),
+  kIsWeb ? const PostScreen() : const EventsCalendarScreen(),
   kIsWeb
       ? const Center(child: Text('Manage Events'))
       : const Center(child: Text('Announcements')),
   kIsWeb
       ? const Center(child: Text('Manage Users'))
       : const Center(child: Text('Personal Events')),
-  kIsWeb
-      ? const Center(child: Text('Settings'))
-      : const ProfileScreen(),
+  kIsWeb ? const Center(child: Text('Settings')) : const ProfileScreen(),
+  const Center(child: Text('Notifications'))
 ];
