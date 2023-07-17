9d16e01832fa5ec99da8c3e55b087766e7cbceb7
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-07-17 17:04:52 +0800

    Refactor `getUserDetailsByEventsCreatedBy` method

--- lib/resources/firestore_user_methods.dart
+++ lib/resources/firestore_user_methods.dart
@@ -9,12 +9,9 @@ class FireStoreUserMethods {
 
   Future<model.User?> getCurrentUserData() async {
     model.User? user;
-
     final currentUser = _auth.currentUser;
     if (currentUser == null) return null;
-
     final documentSnapshot = await _usersCollection.doc(currentUser.uid).get();
-
     if (documentSnapshot.exists) {
       // Convert DocumentSnapshot to User
       user = model.User.fromSnap(documentSnapshot);
@@ -23,44 +20,36 @@ class FireStoreUserMethods {
   }
 
   Future<void> updateCurrentUserData(model.User user) async {
-    await _usersCollection.doc(user.uid).set(user.toJson(), SetOptions(merge: true));
+    await _usersCollection
+        .doc(user.uid)
+        .set(user.toJson(), SetOptions(merge: true));
   }
 
   Future<model.User> getUserDetailsByEventsCreatedBy(String createdBy) async {
-  // Query the events collection for events created by the user
-  QuerySnapshot eventQuerySnapshot = await FirebaseFirestore.instance
-      .collection('events')
-      .where('createdBy', isEqualTo: createdBy)
-      .get();
-
-  // If no events were found, return null
-  if (eventQuerySnapshot.docs.isEmpty) {
-    throw Exception('No events found created by user $createdBy');
+    // Query the events collection for events created by the user
+    QuerySnapshot eventQuerySnapshot = await FirebaseFirestore.instance
+        .collection('events')
+        .where('createdBy', isEqualTo: createdBy)
+        .get();
+    // If no events were found, return null
+    if (eventQuerySnapshot.docs.isEmpty) {
+      throw Exception('No events found created by user $createdBy');
+    }
+    // Get the first event document
+    DocumentSnapshot eventDocument = eventQuerySnapshot.docs.first;
+    // Get the 'createdBy' field from the event document
+    String userId = eventDocument['createdBy'];
+    // Query the users collection for the user with the obtained 'createdBy' (userId)
+    DocumentSnapshot userSnapshot =
+        await FirebaseFirestore.instance.collection('users').doc(userId).get();
+    // If no user was found, return null
+    if (!userSnapshot.exists) {
+      throw Exception('No user found with id $userId');
+    }
+    // Create a User object from the document snapshot
+    model.User user = model.User.fromSnap(userSnapshot);
+    // Return the User object
+    return user;
   }
-
-  // Get the first event document
-  DocumentSnapshot eventDocument = eventQuerySnapshot.docs.first;
-
-  // Get the 'createdBy' field from the event document
-  String userId = eventDocument['createdBy'];
-
-  // Query the users collection for the user with the obtained 'createdBy' (userId)
-  DocumentSnapshot userSnapshot = await FirebaseFirestore.instance
-      .collection('users')
-      .doc(userId)
-      .get();
-
-  // If no user was found, return null
-  if (!userSnapshot.exists) {
-    throw Exception('No user found with id $userId');
-  }
-
-  // Create a User object from the document snapshot
-  model.User user = model.User.fromSnap(userSnapshot);
-
-  // Return the User object
-  return user;
-}
-
 
 }
