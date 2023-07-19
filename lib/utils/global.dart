bff14d22d7820ba056eabece5ea52d29e3b3f7f4
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-07-19 21:13:29 +0800

    Add manage users page for admin
    Signed-off-by: Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>

--- lib/utils/global.dart
+++ lib/utils/global.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:student_event_calendar/screens/feed_screen.dart';
+import 'package:student_event_calendar/screens/manage_users_screen.dart';
 import 'package:student_event_calendar/screens/post_screen.dart';
 import 'package:student_event_calendar/screens/profile_screen.dart';
 import 'package:student_event_calendar/screens/events_calendar_screen.dart';
@@ -23,7 +24,7 @@ List<Widget> homeScreenItems = [
       ? const FeedScreen()
       : const Center(child: Text('Announcements')),
   kIsWeb
-      ? const Center(child: Text('Manage Users'))
+      ? const ManageUsersScreen()
       : const Center(child: Text('Personal Events')),
   kIsWeb ? const Center(child: Text('Settings')) : const ProfileScreen(),
   const Center(child: Text('Notifications'))
