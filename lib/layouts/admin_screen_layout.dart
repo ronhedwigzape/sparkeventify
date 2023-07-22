77244ed4e82b3ee96f657eb307a8010148a9fc63
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-07-22 10:15:48 +0800

    Remove signout and darkmode button at navbar
    Signed-off-by: Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>

--- lib/layouts/admin_screen_layout.dart
+++ lib/layouts/admin_screen_layout.dart
@@ -45,61 +45,6 @@ class _AdminScreenLayoutState extends State<AdminScreenLayout> {
     });
   }
 
-  _signOut() async {
-    return showDialog(
-      context: context,
-      builder: (context) {
-        final darkModeOn = Provider.of<DarkModeProvider>(context, listen: false).darkMode;
-        return SimpleDialog(
-          title: Text(
-            'Log Out Confirmation',
-            style: TextStyle(
-              color: Colors.red[900],
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          children: [
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-              child: Text('Are you sure you want to sign out?'),
-            ),
-            SimpleDialogOption(
-              padding: const EdgeInsets.all(20),
-              onPressed: () async {
-                await AuthMethods().signOut();
-                if (mounted) {
-                  Navigator.of(context).pop();
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => const LoginScreen()));
-                }
-              },
-              child: Row(
-                children: <Widget>[
-                  Icon(Icons.check_circle, color: darkModeOn ? darkModeGrassColor : lightModeGrassColor),
-                  const SizedBox(width: 10),
-                  const Text('Yes'),
-                ],
-              ),
-            ),
-            SimpleDialogOption(
-              padding: const EdgeInsets.all(20),
-              child: Row(
-                children: <Widget>[
-                  Icon(Icons.cancel, color: darkModeOn ? darkModeMaroonColor : lightModeMaroonColor),
-                  const SizedBox(width: 10),
-                  const Text('Go Back'),
-                ],
-              ),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      }
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     final darkModeOn = Provider.of<DarkModeProvider>(context).darkMode;
@@ -120,11 +65,6 @@ class _AdminScreenLayoutState extends State<AdminScreenLayout> {
                 elevation: 0.0,
                 backgroundColor: lightModeTertiaryColor,
                 actions: [
-                  IconButton(
-                    onPressed: () => Provider.of<DarkModeProvider>(context, listen: false).toggleTheme(),
-                    icon: Icon(darkModeOn ? Icons.dark_mode : Icons.light_mode, color: darkModeOn ? darkModeSecondaryColor : lightModeSecondaryColor),
-                    tooltip: 'Switch to ${darkModeOn ? 'Light' : 'Dark'} Mode',
-                  ),
                   buildIconButton(Icons.dashboard, 0, 'Dashboard'),
                   buildIconButton(Icons.add_circle_sharp, 1, 'Post'),
                   buildIconButton(
@@ -132,12 +72,6 @@ class _AdminScreenLayoutState extends State<AdminScreenLayout> {
                   buildIconButton(
                       Icons.supervised_user_circle_sharp, 3, 'Users'),
                   buildIconButton(Icons.settings, 4, 'Settings'),
-                  IconButton(
-                    onPressed: _signOut,
-                    icon: Icon(Icons.logout,
-                        color: darkModeOn ? darkModeMaroonColor : lightModeMaroonColor),
-                    tooltip: 'Log out',
-                  ),
                   Row(
                     children: [
                       Padding(
