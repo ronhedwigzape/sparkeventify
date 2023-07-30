f377fdde3d1e03eeb817d8b6e485c11414dd58dd
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-07-30 18:01:39 +0800

    test: add new test in login_test.dart

--- test/login_test.dart
+++ test/login_test.dart
@@ -1,6 +1,16 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
-import 'mock_auth_repository.dart';
+import 'package:student_event_calendar/data/auth_repository.dart';
+
+class MockAuthRepository extends Mock implements AuthRepository {
+  @override
+  Future<String> signIn({required String email, required String password}) =>
+      super.noSuchMethod(
+        Invocation.method(#signIn, [], {#email: email, #password: password}),
+        returnValue: Future.value('Success'),
+        returnValueForMissingStub: Future.value('Failure'),
+      );
+}
 
 void main() {
   late MockAuthRepository mockAuthRepository;
