57093d4f2a77b2cd9e965d3499636ad3b150f749
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-08-04 19:04:45 +0800

    fix(notification): add function for fetching
    Fix the scrolling behaviour for notification screen

--- lib/screens/notification_screen.dart
+++ lib/screens/notification_screen.dart
@@ -3,23 +3,31 @@ import 'package:flutter/material.dart';
 import '../utils/global.dart';
 import '../widgets/notification_card.dart';
 import '../models/notification.dart' as model;
+import '../models/user.dart' as model;
 
 class NotificationScreen extends StatelessWidget {
   const NotificationScreen({super.key});
+
+  Future<List<model.Notification>> fetchNotifications() async {
+    final snaps = await FirebaseFirestore.instance.collection('notifications').orderBy('timestamp', descending: true).get();
+    final notifications = <model.Notification>[];
+    for (final snap in snaps.docs) {
+      final notification = model.Notification.fromSnap(snap);
+      final senderSnap = await notification.sender!.get();
+      notification.senderData = model.User.fromSnap(senderSnap);
+      notifications.add(notification);
+    }
+    return notifications;
+  }
 
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      body: StreamBuilder<List<model.Notification>>(
-        stream: FirebaseFirestore.instance.collection('notifications').orderBy('timestamp', descending: true).snapshots().map(
-              (snap) => snap.docs.map(
-                  (doc) => model.Notification.fromSnap(doc)
-          ).toList(),
-        ),
-        builder: (BuildContext context,
-            AsyncSnapshot<List<model.Notification>> snapshot) {
+      body: FutureBuilder<List<model.Notification>>(
+        future: fetchNotifications(),
+        builder: (BuildContext context, AsyncSnapshot<List<model.Notification>> snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(
               child: CircularProgressIndicator(),
@@ -27,15 +35,14 @@ class NotificationScreen extends StatelessWidget {
           }
 
           return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) => Container(
-                  margin: EdgeInsets.symmetric(
-                      horizontal: width > webScreenSize ? width * 0.2 : 0,
-                      vertical: width > webScreenSize ? 15 : 0),
-                  child: NotificationCard(
-                    snap: snapshot.data![index],
-                  )
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) => Container(
+              margin: EdgeInsets.symmetric(
+                horizontal: width > webScreenSize ? width * 0.2 : 0,
+                vertical: width > webScreenSize ? 15 : 0),
+              child: NotificationCard(snap: snapshot.data![index],
               )
+            )
           );
         },
       ),
--- lib/widgets/notification_card.dart
+++ lib/widgets/notification_card.dart
@@ -14,82 +14,88 @@ class NotificationCard extends StatefulWidget {
 }
 
 class _NotificationCardState extends State<NotificationCard> {
+  model.User? senderData; // Declare senderData variable
 
   @override
   void initState() {
     super.initState();
+    loadSenderData(); // Load senderData
+  }
+
+  void loadSenderData() {
+    widget.snap.sender?.get().then((snapshot) {
+      setState(() {
+        senderData = model.User.fromSnap(snapshot);
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     final darkModeOn = Provider.of<DarkModeProvider>(context).darkMode;
-    return StreamBuilder<model.User>(
-        stream: widget.snap.sender.snapshots().map<model.User>((snap) => model.User.fromSnap(snap)), // casting the mapped function with User type
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return SizedBox(
-              height: 380.0,
-              child: Center(
-                child: CircularProgressIndicator(color: darkModeOn ? darkModePrimaryColor : lightModePrimaryColor),
-              ),
-            );
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else {
-            model.User? senderData = snapshot.data;
-            return Container(
-              decoration: BoxDecoration(border: Border.all(color: darkModeOn ? secondaryDarkColor : lightColor), color: darkModeOn ? darkColor : lightColor),
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: Column(
-                  children: <Widget>[
-                    Container(
-                      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16).copyWith(right: 0),
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(2, 2, 2, 8),
-                        child: Row(
-                            children: <Widget>[
-                              CircleAvatar(
-                                backgroundImage: NetworkImage(senderData?.profile?.profileImage ?? 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/2048px-Windows_10_Default_Profile_Picture.svg.png'), // Assuming 'senderImageUrl' is a field in your document
-                              ),
-                              const SizedBox(width: 10),
-                              Text(
-                                '${widget.snap.title}', // Assuming 'title' is a field in your document
-                                style: const TextStyle(fontWeight: FontWeight.bold),
-                              ),
-                            ]
-                        ),
-                      ),
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: darkModeOn ? secondaryDarkColor : lightColor,
+        ),
+        color: darkModeOn ? darkColor : lightColor,
+      ),
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16)
+                .copyWith(right: 0),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(2, 2, 2, 8),
+              child: Row(
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(
+                      senderData?.profile?.profileImage ??
+                          'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b5/Windows_10_Default_Profile_Picture.svg/2048px-Windows_10_Default_Profile_Picture.svg.png',
                     ),
-
-                    Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 16),
-                      child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              width: double.infinity,
-                              padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
-                              child: RichText(
-                                  text: TextSpan(
-                                      style: TextStyle(color: darkModeOn ? lightColor : darkColor),
-                                      children: [
-                                        const TextSpan(
-                                          text: 'Description: ',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                        TextSpan(
-                                          text:
-                                          '${widget.snap.message ?? 'default_description'}',
-                                        ),
-                                      ])),
-                            ),
-                          ]),
-                    )
-                  ]),
-            );
-          }
-        });
+                  ),
+                  const SizedBox(width: 10),
+                  Text(
+                    '${widget.snap.title}',
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: RichText(
+                    text: TextSpan(
+                      style: TextStyle(
+                        color: darkModeOn ? lightColor : darkColor,
+                      ),
+                      children: [
+                        const TextSpan(
+                          text: 'Description: ',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(
+                          text: '${widget.snap.message ?? 'default_description'}',
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
