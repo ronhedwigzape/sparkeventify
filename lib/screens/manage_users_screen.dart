5eb77af4503743ace2b93251e1506a2beddd9c7f
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2023-08-19 23:50:02 +0800

    fix(manage_users_screen.dart): add SingleChildScrollView widget
    Fix issue #4

--- lib/screens/manage_users_screen.dart
+++ lib/screens/manage_users_screen.dart
@@ -109,178 +109,180 @@ class _ManageUsersScreenState extends State<ManageUsersScreen> {
 
         return Padding(
           padding: const EdgeInsets.fromLTRB(13, 15, 13, 0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 14),
-                child: TextField(
-                  controller: searchController,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.search),
-                    labelText: "Search for users",
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 14),
+                  child: TextField(
+                    controller: searchController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.search),
+                      labelText: "Search for users",
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 100),
-                child: SizedBox(
-                  height: 50.0,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.manage_accounts,
-                            color: darkModeOn ? lightColor : darkColor,
-                            size: 40,
-                          ),
-                          const SizedBox(width: 5,),
-                          Text(
-                            'Manage Users',
-                            style: TextStyle(
-                                fontSize: 32.0,
-                                fontWeight: FontWeight.bold,
-                                color: darkModeOn ? lightColor : darkColor
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 100),
+                  child: SizedBox(
+                    height: 50.0,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.manage_accounts,
+                              color: darkModeOn ? lightColor : darkColor,
+                              size: 40,
                             ),
-                          ),
-                        ],
-                      ),
+                            const SizedBox(width: 5,),
+                            Text(
+                              'Manage Users',
+                              style: TextStyle(
+                                  fontSize: 32.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: darkModeOn ? lightColor : darkColor
+                              ),
+                            ),
+                          ],
+                        ),
+                        
+                       
+                        DropdownButton<String>(
+                          value: dropdownUserType,
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              dropdownUserType = newValue!;
+                            });
+                          },
+                          items: <String>['All', 'Student', 'Officer', 'Staff']
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
                       
-                     
-                      DropdownButton<String>(
-                        value: dropdownUserType,
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            dropdownUserType = newValue!;
-                          });
-                        },
-                        items: <String>['All', 'Student', 'Officer', 'Staff']
-                            .map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                      ),
-                    
-                      DropdownButton<String>(
-                        value: dropdownYear,
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            dropdownYear = newValue!;
-                          });
-                        },
-                        items: <String>['All', '1', '2', '3', '4']
-                            .map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                      ),
-                     
-                      DropdownButton<String>(
-                        value: dropdownDepartment,
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            dropdownDepartment = newValue!;
-                          });
-                        },
-                        items: <String>['All', 'CCS', 'CHS']
-                            .map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                      ),
-                      
-                      DropdownButton<String>(
-                        value: dropdownCourse,
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            dropdownCourse = newValue!;
-                          });
-                        },
-                        items: <String>['All', 'BSIT', 'BSN', 'BSCS']
-                            .map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                      ),
-                      
-                      DropdownButton<String>(
-                        value: dropdownSection,
-                        onChanged: (String? newValue) {
-                          setState(() {
-                            dropdownSection = newValue!;
-                          });
-                        },
-                        items: <String>['All', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
-                            .map<DropdownMenuItem<String>>((String value) {
-                          return DropdownMenuItem<String>(
-                            value: value,
-                            child: Text(value),
-                          );
-                        }).toList(),
-                      ),
-                      NotificationButton(selectedUsers: selectedUsers, clearSelectedUsers: clearSelectedUsers),
-                      Checkbox(
-                        activeColor: darkModeOn ? darkModePrimaryColor : lightModePrimaryColor,
-                        checkColor: darkModeOn ? darkColor : lightColor,
-                        value: _allFilteredUsersSelected,
-                        onChanged: (bool? value) {
-                          setState(() {
-                            toggleAllSelected();
-                          });
-                        },
-                      ),
-                    ],
+                        DropdownButton<String>(
+                          value: dropdownYear,
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              dropdownYear = newValue!;
+                            });
+                          },
+                          items: <String>['All', '1', '2', '3', '4']
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
+                       
+                        DropdownButton<String>(
+                          value: dropdownDepartment,
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              dropdownDepartment = newValue!;
+                            });
+                          },
+                          items: <String>['All', 'CCS', 'CHS']
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
+                        
+                        DropdownButton<String>(
+                          value: dropdownCourse,
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              dropdownCourse = newValue!;
+                            });
+                          },
+                          items: <String>['All', 'BSIT', 'BSN', 'BSCS']
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
+                        
+                        DropdownButton<String>(
+                          value: dropdownSection,
+                          onChanged: (String? newValue) {
+                            setState(() {
+                              dropdownSection = newValue!;
+                            });
+                          },
+                          items: <String>['All', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+                              .map<DropdownMenuItem<String>>((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
+                        NotificationButton(selectedUsers: selectedUsers, clearSelectedUsers: clearSelectedUsers),
+                        Checkbox(
+                          activeColor: darkModeOn ? darkModePrimaryColor : lightModePrimaryColor,
+                          checkColor: darkModeOn ? darkColor : lightColor,
+                          value: _allFilteredUsersSelected,
+                          onChanged: (bool? value) {
+                            setState(() {
+                              toggleAllSelected();
+                            });
+                          },
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
-                child: Text(
-                  'Instructions: In this section, all users can be viewed. Notify them for events & announcements. You can update and delete the users account information here. ',
-                  style: TextStyle(
-                      fontSize: 15.0,
-                      color: darkModeOn ? darkModeTertiaryColor : lightModeTertiaryColor
+                Container(
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: Text(
+                    'Instructions: In this section, all users can be viewed. Notify them for events & announcements. You can update and delete the users account information here. ',
+                    style: TextStyle(
+                        fontSize: 15.0,
+                        color: darkModeOn ? darkModeTertiaryColor : lightModeTertiaryColor
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height > webScreenSize ? width * 0.33 : 0,
-                child: SingleChildScrollView(
-                  child: Column(
-                  children: filteredUsers.isEmpty
-                  ? <Widget>[const Text('No user matches your search.')]  // If filteredUsers is empty, display a text message
-                  : filteredUsers.map((user) => Container(  // Otherwise, display the list of users
-                    margin: EdgeInsets.symmetric(
-                    horizontal: width > webScreenSize ? width * 0.08 : 0,
-                    vertical: width > webScreenSize ? 7 : 0),
-                      child: UsersCard(
-                        user: user,
-                        selectedUsers: selectedUsers,
-                        onSelectedChanged: (uid) {
-                          setState(() {
-                          if (selectedUsers.contains(uid)) {
-                            selectedUsers.remove(uid);
-                          } else {
-                            selectedUsers.add(uid);
-                        }
-                      });
-                      })
-                    )).toList(),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height > webScreenSize ? width * 0.33 : 0,
+                  child: SingleChildScrollView(
+                    child: Column(
+                    children: filteredUsers.isEmpty
+                    ? <Widget>[const Text('No user matches your search.')]  // If filteredUsers is empty, display a text message
+                    : filteredUsers.map((user) => Container(  // Otherwise, display the list of users
+                      margin: EdgeInsets.symmetric(
+                      horizontal: width > webScreenSize ? width * 0.08 : 0,
+                      vertical: width > webScreenSize ? 7 : 0),
+                        child: UsersCard(
+                          user: user,
+                          selectedUsers: selectedUsers,
+                          onSelectedChanged: (uid) {
+                            setState(() {
+                            if (selectedUsers.contains(uid)) {
+                              selectedUsers.remove(uid);
+                            } else {
+                              selectedUsers.add(uid);
+                          }
+                        });
+                        })
+                      )).toList(),
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         );
       },
