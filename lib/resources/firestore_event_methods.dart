3e4b290f6ac892c27031d80f782cbbc9510a9084
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2023-08-24 21:16:33 +0800

    feat: add a documentation on code

--- lib/resources/firestore_event_methods.dart
+++ lib/resources/firestore_event_methods.dart
@@ -107,30 +107,45 @@ class FireStoreEventMethods {
 
   // Method that has a key of type event's DateTime and a value of type List<Event>
   Future<Map<DateTime, List<Event>>> getEventsByDate() async {
+    // Initialize an empty map to store the events.
     Map<DateTime, List<Event>> events = {};
+
+    // Get all documents from the events collection.
     QuerySnapshot snapshot = await _eventsCollection.get();
 
+    // Check if the snapshot contains any documents.
     if (snapshot.docs.isNotEmpty) {
+      // Loop through each document in the snapshot.
       for (var doc in snapshot.docs) {
+        // Convert the document snapshot to an Event object.
         Event event = await Event.fromSnap(doc);
 
+        // Get the start and end dates of the event and adjust the time to the start and end of the day respectively.
         DateTime startDate = DateTime(event.startDate.year, event.startDate.month, event.startDate.day, 0, 0, 0)
             .toLocal();
         DateTime endDate = DateTime(event.endDate.year, event.endDate.month, event.endDate.day, 23, 59, 59)
             .toLocal();
 
+        // Loop through each day between the start and end dates.
         for (var day = startDate; day.isBefore(endDate) || day.isAtSameMomentAs(endDate);
         day = day.add(const Duration(days: 1))) {
+          // Adjust the time of the day to the start of the day.
           DateTime adjustedDay = DateTime(day.year, day.month, day.day, 0, 0, 0)
               .toLocal();
+
+          // Check if the events map already contains the adjusted day as a key.
           if (events.containsKey(adjustedDay)) {
+            // If the key exists, add the event to the list of events for that day.
             events[adjustedDay]!.add(event);
           } else {
+            // If the key does not exist, create a new list with the event and add it to the map.
             events[adjustedDay] = [event];
           }
         }
       }
     }
+
+    // Return the map of events.
     return events;
   }
 
