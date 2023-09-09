4e2064eb73779e4c0395b87c7dc6fb89a6140598
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2023-09-09 20:18:15 +0800

    feat: add new methods for feedbacks

--- lib/resources/firestore_feedback_methods.dart
+++ lib/resources/firestore_feedback_methods.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:student_event_calendar/models/evaluator_feedback.dart';
 import 'package:student_event_calendar/models/event_feedbacks.dart';
@@ -8,25 +10,56 @@ final FirebaseFirestore _db = FirebaseFirestore.instance;
 CollectionReference _eventRef = _db.collection('events');
 
 class FirestoreFeedbackMethods {
+  // Method to get eventFeedbackUid by eventId
 
-  // Method to check if an event feedback collection is present
-  Future<bool> isEventFeedbackPresent(String eventId, String feedbackId) async {
-    DocumentSnapshot snapshot = await _eventRef.doc(eventId).collection('feedbacks').doc(feedbackId).get();
-    return snapshot.exists;
+  Stream<String?> streamGetEventFeedbackUid(String eventId) async* {
+    final controller = StreamController<String?>();
+
+    _eventRef.doc(eventId)
+      .collection('feedbacks')
+      .get()
+      .then((snapshot) {
+        if (snapshot.docs.isNotEmpty) {
+          var data = snapshot.docs.first.data();
+          controller.add(data['eventFeedbackUid']);
+        } else {
+          controller.add(null);
+        }
+        controller.close();
+      })
+      .catchError((error) {
+        controller.addError(error);
+        return null;  // Return null instead of void
+      });
+
+    yield* controller.stream;
+  }
+
+  Future<String?> getEventFeedbackUid(String eventId) async {
+    QuerySnapshot snapshot =
+        await _eventRef.doc(eventId).collection('feedbacks').get();
+    if (snapshot.docs.isNotEmpty) {
+      var data = snapshot.docs.first.data() as Map<String, dynamic>;
+      return data['eventFeedbackUid'];
+    } else {
+      return null;
+    }
   }
 
   // Method to add a feedback to a specific event
-  Future<void> addEmptyFeedback(String eventId) {
+  Future<String> addEmptyFeedback(String eventId) async {
     var uuid = const Uuid();
     String feedbackId = uuid.v4();
 
-    return _eventRef.doc(eventId)
+    await _eventRef.doc(eventId)
       .collection('feedbacks')
       .doc(feedbackId)
       .set({
         'eventFeedbackUid': feedbackId,
         'evaluatorFeedbacks': []
       });
+
+    return feedbackId;
   }
 
   // Method to add evaluation to the feedback
@@ -72,35 +105,107 @@ class FirestoreFeedbackMethods {
     List<EventFeedbacks> allFeedbacks = await getAllFeedbacks(eventId);
     List<EvaluatorFeedback> userFeedbacks = [];
     for (var feedback in allFeedbacks) {
-      userFeedbacks.addAll(feedback.evaluatorFeedbacks.where((evaluatorFeedback) => evaluatorFeedback.userUid == userId));
+      userFeedbacks.addAll(feedback.evaluatorFeedbacks
+        .where((evaluatorFeedback) => evaluatorFeedback.userUid == userId));
     }
     return userFeedbacks;
+  }
+
+  Stream<bool> getEventFeedbackStatusByUserId(String eventId, String userId) {
+    return _eventRef
+        .doc(eventId)
+        .collection('feedbacks')
+        .snapshots()
+        .map((snapshot) {
+      bool isFeedbackDone = false;
+
+      for (var doc in snapshot.docs) {
+        EventFeedbacks feedback =
+            EventFeedbacks.fromMap(doc.data());
+        for (var evaluatorFeedback in feedback.evaluatorFeedbacks) {
+          if (evaluatorFeedback.userUid == userId) {
+            isFeedbackDone = evaluatorFeedback.isFeedbackDone;
+            break; // Break the loop if the user's feedback is found
+          }
+        }
+      }
+
+      return isFeedbackDone;
+    });
+  }
+
+  // Method to unlock the feedback for a specific user
+  Future<void> unlockEvaluatorEventFeedbackByUserId(
+    String eventId,
+    String userId,
+  ) async {
+    List<EventFeedbacks> allFeedbacks = await getAllFeedbacks(eventId);
+
+    for (var feedback in allFeedbacks) {
+      feedback.evaluatorFeedbacks =
+          feedback.evaluatorFeedbacks.map((evaluatorFeedback) {
+        if (evaluatorFeedback.userUid == userId) {
+          evaluatorFeedback.isFeedbackDone = false;
+        }
+        return evaluatorFeedback;
+      }).toList();
+
+      await _eventRef
+          .doc(eventId)
+          .collection('feedbacks')
+          .doc(feedback.eventFeedbackUid)
+          .update(feedback.toJson());
+    }
+  }
+
+  // Method to unlock all feedback for a specific event
+  Future<void> unlockAllFeedbacksInEvent(String eventId) async {
+    List<EventFeedbacks> allFeedbacks = await getAllFeedbacks(eventId);
+
+    for (var feedback in allFeedbacks) {
+      feedback.evaluatorFeedbacks =
+          feedback.evaluatorFeedbacks.map((evaluatorFeedback) {
+        evaluatorFeedback.isFeedbackDone = false;
+        return evaluatorFeedback;
+      }).toList();
+
+      await _eventRef
+          .doc(eventId)
+          .collection('feedbacks')
+          .doc(feedback.eventFeedbackUid)
+          .update(feedback.toJson());
+    }
   }
 
   // Method to remove evaluator's feedback basing from current user's id
   Future<void> removeEvaluatorsFeedbackByUserId(String eventId, String userId) async {
     List<EventFeedbacks> allFeedbacks = await getAllFeedbacks(eventId);
     for (var feedback in allFeedbacks) {
-      feedback.evaluatorFeedbacks.removeWhere((evaluatorFeedback) => evaluatorFeedback.userUid == userId);
-      _eventRef.doc(eventId).collection('feedbacks').doc(feedback.eventFeedbackUid).update(feedback.toJson());
+      feedback.evaluatorFeedbacks.removeWhere(
+          (evaluatorFeedback) => evaluatorFeedback.userUid == userId);
+      _eventRef
+          .doc(eventId)
+          .collection('feedbacks')
+          .doc(feedback.eventFeedbackUid)
+          .update(feedback.toJson());
     }
   }
 
   // Method to remove event feedback
   Future<void> removeFeedback(String eventId, String feedbackId) {
-    return _eventRef.doc(eventId)
-      .collection('feedbacks')
-      .doc(feedbackId)
-      .delete();
+    return _eventRef
+        .doc(eventId)
+        .collection('feedbacks')
+        .doc(feedbackId)
+        .delete();
   }
 
   // Method to remove all event feedbacks
   Future<void> removeAllEventFeedbacks(String eventId) async {
-    QuerySnapshot snapshot = await _eventRef.doc(eventId).collection('feedbacks').get();
+    QuerySnapshot snapshot =
+        await _eventRef.doc(eventId).collection('feedbacks').get();
     for (QueryDocumentSnapshot doc in snapshot.docs) {
       doc.reference.delete();
     }
   }
-
-
 }
