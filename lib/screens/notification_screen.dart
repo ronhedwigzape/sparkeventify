6368225bb58cba332f8e9171aaa810a6c4996aca
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2023-09-15 14:28:58 +0800

    fix(notification-screen): display notification not found

--- lib/screens/notification_screen.dart
+++ lib/screens/notification_screen.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:student_event_calendar/providers/darkmode_provider.dart';
@@ -11,17 +13,25 @@ import '../models/user.dart' as model;
 class NotificationScreen extends StatelessWidget {
   const NotificationScreen({super.key});
 
-  Future<List<model.Notification>> fetchNotifications() async {
-    final snaps = await FirebaseFirestore.instance.collection('notifications').orderBy('timestamp', descending: true).get();
-    final notifications = <model.Notification>[];
-    for (final snap in snaps.docs) {
-      final notification = model.Notification.fromSnap(snap);
-      final senderSnap = await notification.sender!.get();
-      notification.senderData = model.User.fromSnap(senderSnap);
-      notifications.add(notification);
-    }
-    return notifications;
+Future<List<model.Notification>> fetchNotifications() async {
+  final currentUserID = FirebaseAuth.instance.currentUser!.uid;
+
+  // Only get notifications for the current user
+  final snaps = await FirebaseFirestore.instance
+      .collection('notifications')
+      .where('recipient', isEqualTo: FirebaseFirestore.instance.doc('users/$currentUserID'))
+      .orderBy('timestamp', descending: true)
+      .get();
+  final notifications = <model.Notification>[];
+  for (final snap in snaps.docs) {
+    final notification = model.Notification.fromSnap(snap);
+    final senderSnap = await notification.sender!.get();
+    notification.senderData = model.User.fromSnap(senderSnap);
+    notifications.add(notification);
   }
+  return notifications;
+}
+
 
   @override
   Widget build(BuildContext context) {
@@ -33,12 +43,39 @@ class NotificationScreen extends StatelessWidget {
         future: fetchNotifications(),
         builder: (BuildContext context, AsyncSnapshot<List<model.Notification>> snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
+            return Center(child: CircularProgressIndicator(color: darkModeOn ? darkModePrimaryColor : lightModePrimaryColor));
           }
-
-          return snapshot.data!.isNotEmpty ? ListView.builder(
+          else if (snapshot.hasError) {
+            if (kDebugMode) {
+              print(snapshot.error);
+            }
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } 
+          if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(
+              child: Padding(
+                padding: const EdgeInsets.all(50.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.notification_important_rounded, size: 25.0, color: darkModeOn ? darkModeSecondaryColor : lightModeSecondaryColor,),
+                        const Flexible(
+                          child: Text(
+                            'You don’t have any notifications. Check back later.',
+                            textAlign: TextAlign.center,
+                            )),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ); 
+          }
+          
+          return ListView.builder(
             itemCount: snapshot.data!.length,
             itemBuilder: (context, index) => Container(
               margin: EdgeInsets.symmetric(
@@ -47,23 +84,6 @@ class NotificationScreen extends StatelessWidget {
               child: NotificationCard(snap: snapshot.data![index],
               )
             )
-          ) : Center(
-            child: Padding(
-              padding: const EdgeInsets.all(50.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.notification_important_rounded, size: 20.0, color: darkModeOn ? darkModeSecondaryColor : lightModeSecondaryColor,),
-                      const Text('No recent updates.'),
-                    ],
-                  ),
-                ],
-              ),
-            ),
           );
         },
       ),
