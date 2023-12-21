cd9b15be22b59fb9d809ca4d0f270929d9437e99
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2023-12-21 17:48:54 +0800

    feat: add pie chart feedback and table data of attendance
    Add a graphical representation of feedbacks
    Add a tabular data of attendance

--- lib/screens/event_feedback_screen.dart
+++ lib/screens/event_feedback_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:student_event_calendar/models/evaluator_feedback.dart';
 import 'package:student_event_calendar/models/event_feedbacks.dart';
 import 'package:student_event_calendar/providers/darkmode_provider.dart';
 import 'package:student_event_calendar/resources/firestore_feedback_methods.dart';
 import 'package:student_event_calendar/utils/colors.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class EventFeedbackScreen extends StatefulWidget {
   const EventFeedbackScreen({super.key, required this.eventId});
@@ -16,56 +16,183 @@ class EventFeedbackScreen extends StatefulWidget {
 }
 
 class _EventFeedbackScreenState extends State<EventFeedbackScreen> {
+  String? attendanceFilter;
+  String? satisfactionFilter;
+  String? programFilter;
+  String? departmentFilter;
+  List<DataRow> rows = [];
+  List<DataRow> filteredRows = [];
+
+  void filterRows() {
+    setState(() {
+      filteredRows = rows.where((row) {
+        final programCell = row.cells[1].child as Text;
+        final departmentCell = row.cells[2].child as Text;
+        final attendanceCell = row.cells[3].child as Text;
+        final satisfactionCell = row.cells[4].child as Text;
+        return (attendanceFilter == null ||
+                attendanceCell.data == attendanceFilter) &&
+            (satisfactionFilter == null ||
+                satisfactionCell.data == satisfactionFilter) &&
+            (programFilter == null || programCell.data == programFilter) &&
+            (departmentFilter == null ||
+                departmentCell.data == departmentFilter);
+      }).toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final darkModeOn = Provider.of<DarkModeProvider>(context).darkMode;
     return Scaffold(
-      appBar: AppBar(title: const Text('Event Feedback Evaluation')),
-      body: FutureBuilder<List<EventFeedbacks>>(
-        future: FirestoreFeedbackMethods().getAllFeedbacks(widget.eventId),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: LinearProgressIndicator(color: darkModeOn ? darkModePrimaryColor : lightModePrimaryColor,));
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else {
-            if (snapshot.data!.isEmpty) {
-              return const Center(child: Text('No feedbacks for this event.'));
-            } else {
-            return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                EventFeedbacks eventFeedbacks = snapshot.data![index];
-                return ListView.builder(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemCount: eventFeedbacks.evaluatorFeedbacks.length,
-                  itemBuilder: (context, feedbackIndex) {
-                    EvaluatorFeedback feedback = eventFeedbacks.evaluatorFeedbacks[feedbackIndex];
-                    return Card(
-                      child: ListTile(
-                        title: Text(feedback.userFullName),
-                        subtitle: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Program: ${feedback.userProgram}',style: TextStyle(color: darkModeOn ? lightColor : darkColor),),
-                            Text('Department: ${feedback.userDepartment}',style: TextStyle(color: darkModeOn ? lightColor : darkColor)),
-                            Text('Satisfaction: ${feedback.satisfactionStatus ? 'Satisfied' : 'Not satisfied'}',style: TextStyle(color: darkModeOn ? lightColor : darkColor)),
-                            Text('Evaluation: ${feedback.studentEvaluation}',style: TextStyle(color: darkModeOn ? lightColor : darkColor)),
-                            Text('Attendance: ${feedback.attendanceStatus ? 'Attended' : 'Did not attend'}',style: TextStyle(color: darkModeOn ? lightColor : darkColor)),
-                            Text('Feedback done: ${feedback.isFeedbackDone ? 'Yes' : 'No'}',style: TextStyle(color: darkModeOn ? lightColor : darkColor)),
-                          ],
+        appBar: AppBar(title: const Text('Event Feedback Evaluation')),
+        body: FutureBuilder<List<EventFeedbacks>>(
+            future: FirestoreFeedbackMethods().getAllFeedbacks(widget.eventId),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                    child: LinearProgressIndicator(
+                  color:
+                      darkModeOn ? darkModePrimaryColor : lightModePrimaryColor,
+                ));
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                if (snapshot.data!.isEmpty) {
+                  return const Center(
+                      child: Text('No feedbacks for this event.'));
+                } else {
+                  int satisfiedCount = 0;
+                  int notSatisfiedCount = 0;
+                  rows = [];
+
+                  for (var feedbacks in snapshot.data!) {
+                    for (var feedback in feedbacks.evaluatorFeedbacks) {
+                      if (feedback.satisfactionStatus!) {
+                        satisfiedCount++;
+                      } else {
+                        notSatisfiedCount++;
+                      }
+
+                      rows.add(DataRow(
+                        cells: <DataCell>[
+                          DataCell(Text(feedback.userFullName!)),
+                          DataCell(Text(feedback.userProgram!)),
+                          DataCell(Text(feedback.userDepartment!)),
+                          DataCell(Text(feedback.attendanceStatus!
+                              ? 'Attended'
+                              : 'Did not attend')),
+                          DataCell(Text(feedback.satisfactionStatus!
+                              ? 'Satisfied'
+                              : 'Not satisfied')),
+                        ],
+                      ));
+                    }
+                  }
+
+                  Map<String, double> dataMap = {
+                    'Satisfied': satisfiedCount.toDouble(),
+                    'Not Satisfied': notSatisfiedCount.toDouble(),
+                  };
+
+                  filteredRows = List.from(rows);
+
+                  return SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 20,),
+                      PieChart(
+                        dataMap: dataMap,
+                        animationDuration: Duration(milliseconds: 800),
+                        chartLegendSpacing: 32,
+                        chartRadius: MediaQuery.of(context).size.width / 3.2,
+                        initialAngleInDegree: 0,
+                        chartType: ChartType.ring,
+                        ringStrokeWidth: 32,
+                        centerText: "Satisfaction",
+                        legendOptions: LegendOptions(
+                          showLegendsInRow: false,
+                          legendPosition: LegendPosition.right,
+                          showLegends: true,
+                          legendShape: BoxShape.circle,
+                          legendTextStyle: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        chartValuesOptions: ChartValuesOptions(
+                          showChartValueBackground: true,
+                          showChartValues: true,
+                          showChartValuesInPercentage: true,
+                          showChartValuesOutside: false,
+                          decimalPlaces: 1,
                         ),
                       ),
-                    );
-                  },
-                );
-              },
-            );
-          }
-          }
-        },
-      ),
-    );
+                      DropdownButton<String>(
+                        value: attendanceFilter,
+                        items: <String>[
+                          'All',
+                          'Attended',
+                          'Did not attend',
+                        ].map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            attendanceFilter =
+                                newValue == 'All' ? null : newValue;
+                            filterRows();
+                          });
+                        },
+                      ),
+                      DropdownButton<String>(
+                        value: satisfactionFilter,
+                        items: <String>[
+                          'All',
+                          'Satisfied',
+                          'Not satisfied',
+                        ].map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            satisfactionFilter =
+                                newValue == 'All' ? null : newValue;
+                            filterRows();
+                          });
+                        },
+                      ),
+                      // Add similar DropdownButtons for programFilter and departmentFilter here
+                      DataTable(
+                        columns: const <DataColumn>[
+                          DataColumn(
+                            label: Text('Name'),
+                          ),
+                          DataColumn(
+                            label: Text('Program'),
+                          ),
+                          DataColumn(
+                            label: Text('Department'),
+                          ),
+                          DataColumn(
+                            label: Text('Attendance'),
+                          ),
+                          DataColumn(
+                            label: Text('Satisfaction'),
+                            numeric: true,
+                          ),
+                        ],
+                        rows: filteredRows,
+                      )
+                    ]),
+                  );
+                }
+              }
+            }));
   }
 }
