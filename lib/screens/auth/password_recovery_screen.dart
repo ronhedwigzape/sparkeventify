aa0bc08ce102edde685a9959ec85c1f897da5d9c
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2024-01-08 03:59:30 +0800

    fix: send password reset link to only users that has account

--- lib/screens/auth/password_recovery_screen.dart
+++ lib/screens/auth/password_recovery_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:student_event_calendar/providers/darkmode_provider.dart';
@@ -25,12 +26,37 @@ class PasswordRecoveryScreenState extends State<PasswordRecoveryScreen> {
 
     if (email.isNotEmpty) {
       if (pattern.hasMatch(email)) {
-        await _authMethods.sendPasswordResetEmail(email);
-        scaffoldMessenger.showSnackBar(const SnackBar(
-          content: Text('Password reset email sent'),
-          duration: Duration(seconds: 2),
-        ));
-        _emailController.clear();
+        // Check if the email exists in the 'users' collection
+        QuerySnapshot userSnapshot = await FirebaseFirestore.instance
+            .collection('users')
+            .where('email', isEqualTo: email)
+            .get();
+
+        // Check if the email exists in the 'trashedUsers' collection
+        QuerySnapshot trashedUserSnapshot = await FirebaseFirestore.instance
+            .collection('trashedUsers')
+            .where('email', isEqualTo: email)
+            .get();
+
+        if (userSnapshot.docs.isEmpty && trashedUserSnapshot.docs.isEmpty) {
+          scaffoldMessenger.showSnackBar(const SnackBar(
+            content: Text('Email address not found.'),
+            duration: Duration(seconds: 2),
+          ));
+        } else if (trashedUserSnapshot.docs.isNotEmpty) {
+          scaffoldMessenger.showSnackBar(const SnackBar(
+            content: Text('This account has been disabled.'),
+            duration: Duration(seconds: 2),
+          ));
+        } else {
+          // Email exists and is not trashed, send password reset email
+          await _authMethods.sendPasswordResetEmail(email);
+          scaffoldMessenger.showSnackBar(const SnackBar(
+            content: Text('Password reset email sent'),
+            duration: Duration(seconds: 2),
+          ));
+          _emailController.clear();
+        }
       } else {
         scaffoldMessenger.showSnackBar(const SnackBar(
           content: Text('Invalid domain name. Use @my.cspc.edu.ph or @cspc.edu.ph'),
