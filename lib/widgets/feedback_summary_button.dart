0144045dd5e6da9c35a2e8223e439014fa47ce3c
Ron Hedwig Zape <ronzape@ronhedwigzape.com>
2024-02-17 18:23:36 +0800

    refactor: remove satisfied and dissatisfied evaluators

--- lib/widgets/feedback_summary_button.dart
+++ lib/widgets/feedback_summary_button.dart
@@ -46,8 +46,6 @@ class _FeedbackSummaryButtonState extends State<FeedbackSummaryButton> {
                       child: ListBody(
                         children: <Widget>[
                           Text('Total Evaluators: ${summary['totalEvaluators']}', style: TextStyle(color: darkModeOn ? lightColor : darkColor),),
-                          Text('Satisfied Evaluators: ${summary['satisfiedEvaluators']}', style: TextStyle(color: darkModeOn ? lightColor : darkColor),),
-                          Text('Dissatisfied Evaluators: ${summary['dissatisfiedEvaluators']}', style: TextStyle(color: darkModeOn ? lightColor : darkColor),),
                           Text('Programs: ${summary['programs'].entries.map((e) => '${e.key}: ${e.value}').join(', ')}', style: TextStyle(color: darkModeOn ? lightColor : darkColor),),
                           Text('Departments: ${summary['departments'].entries.map((e) => '${e.key}: ${e.value}').join(', ')}', style: TextStyle(color: darkModeOn ? lightColor : darkColor),),
                         ],
